417a992f23d50281bf863a9c46e8aafe1e4fbf22
Axel <lezinaxel@gmail.com>
2016-12-02 12:18:17 +0100

    Requête ajax effectuée

--- ajax.js
+++ ajax.js
@@ -7,6 +7,17 @@ console.log('Start');
 		success : function(){
 			var urlServer = window.config.serverUrl + '/filleules.json';
 			$.ajax(urlServer)
+
+			.done(app.showObject)
+			console.log(urlServer);
+		},
+		showObject : function(response){
+			console.log(response);
+			var template = "<span>{{#filleules}} {{prenom}} {{nom}} {{profession}} {{ville}} {{description}} {{modeInteraction}} {{/filleules}}</span>"
+			console.log(template);
+			var rendered = Mustache.render(template, response);
+			console.log(rendered);
+			$('span').append(rendered);
 		}
 	}
 
