3218b9eb27a31c6bd449a5df87c6c2bc03f1f580
Axel <lezinaxel@gmail.com>
2016-12-02 10:59:51 +0100

    Url du serveur ( window.config )

--- ajax.js
+++ ajax.js
@@ -1,1 +1,47 @@
 console.log('Start');
+(function(){
+	var app = {
+		init : function(){
+			app.success();
+		},
+		success : function(){
+			var urlServer = window.config.serverUrl + '/filleules.json';
+			$.ajax(urlServer)
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	app.init();
+})();
